bcab2a3b802ab9e9097cc3a35260d4b87359dee1
CatWithoutSoup <roma.malov2000@mail.ru>
2026-08-13 15:03:24 +0500

    fix NavigationBar bug

--- feature/registration/src/main/java/com/example/registration/RegistrationScreen.kt
+++ feature/registration/src/main/java/com/example/registration/RegistrationScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
@@ -54,6 +56,8 @@ fun RegistrationScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .statusBarsPadding()
+                .navigationBarsPadding()
+                .imePadding()
                 .verticalScroll(scrollState)
                 .padding(
                     start = 24.dp,
